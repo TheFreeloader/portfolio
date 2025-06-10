8360f76c2995a4164c5d43e431678bb559b69b49
Cyryl John Navarro <110725623+TheFreeloader@users.noreply.github.com>
2025-06-10 18:39:48 +0800

    Add SVG assets for various programming languages and frameworks
    - Added Java SVG icon
    - Added JavaScript SVG icon
    - Added Node.js SVG icon
    - Added PHP SVG icon
    - Added Python SVG icon
    - Added React SVG icon
    - Added Tailwind CSS SVG icon
    - Added TypeScript SVG icon
    - Added Vite SVG icon
    - Added Top SVG icon

--- src/components/Card/cardHandler.ts
+++ src/components/Card/cardHandler.ts
@@ -6,7 +6,7 @@ export function initializeCard(cardId: string) {
 
     if (card) {
       const baseClasses =
-        "flex flex-col bg-[#1c1c25] justify-evenly overflow-hidden min-w-[200px] max-w-[200px] min-h-[200px] max-h-[200px] rounded-2xl shadow-[0_16px_32px_0_rgba(16,16,22,1),3px_0_6px_0_rgba(0,0,0,0.2),-3px_0_6px_0_rgba(0,0,0,0.2)] border border-[rgba(0,0,0,0.5)] hover:border-[rgba(250,249,246,0.5)] hover:shadow-[0_2px_6px_rgba(16,16,22,0.6)] hover:translate-y-[8px] active:translate-y-[4px] active:shadow-sm select-none justify-between transition-all duration-150 ease-in-out cursor-pointer";
+        "flex flex-col bg-[#1c1c25] justify-evenly overflow-hidden hover:bg-[#2a2a30] min-w-[220px] max-w-[220px] min-h-[250px] max-h-[250px] rounded-2xl shadow-[0_16px_32px_0_rgba(16,16,22,1),3px_0_6px_0_rgba(0,0,0,0.2),-3px_0_6px_0_rgba(0,0,0,0.2)] border border-[rgba(0,0,0,0.5)] hover:border-[rgba(250,249,246,0.5)] hover:shadow-[0_2px_6px_rgba(16,16,22,0.6)] hover:translate-y-[8px] active:translate-y-[4px] active:shadow-sm select-none justify-between transition-all duration-150 ease-in-out cursor-pointer";
 
       const clickedClasses =
         "flex flex-col bg-[#1c1c25] justify-evenly overflow-hidden min-w-[200px] max-w-[200px] min-h-[200px] max-h-[200px] rounded-xl border border-[rgba(250,249,246,0.5)] shadow-sm translate-y-[4px] select-none justify-between transition-all duration-150 ease-in-out cursor-pointer hover:border-[rgba(250,249,246,0.5)] hover:shadow-sm hover:translate-y-[4px] active:border-[rgba(250,249,246,0.5)] active:shadow-sm active:translate-y-[4px]";
